db79eaa9d7c6b3fbdb93b945f82adaf1eec4db80
ALBERT IMASUEN <studentxapp@gmail.com>
2024-04-15 20:19:00 -0400

    Creating a Separate Component file to handle the navigation menu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import "./App.css";
+import Navigation from "./components/Navigation";
 import { Books } from "./components/books";
 
 function App() {
   return (
     <div>
-      <nav>
-      <div class="nav__content">
-        <div class="logo"><a href="#">MyLibrary</a></div>
-        <label for="check" class="checkbox">
-          <i class="ri-menu-line"></i>
-        </label>
-        <input type="checkbox" name="check" id="check" />
-        <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Resume</a></li>
-          <li><a href="#">Portfolio</a></li>
-          <li><a href="#">Contact</a></li> 
-        </ul>
-      </div>
-    </nav>
+     <Navigation />
 
     <div className="App">
       <div className="col">
