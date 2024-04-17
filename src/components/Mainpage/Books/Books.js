5f646918a3eae81932aef00ac2abfae7fc0deaea
Prince Isaac <isaacprince002@gmail.com>
2024-04-17 18:19:13 +0100

    feat: added link to new card component

--- src/components/Mainpage/Books/Books.js
+++ src/components/Mainpage/Books/Books.js
@@ -6,17 +6,24 @@ const Books = (props) => {
     <div className="books">
       <div className="grid">
         <Card>
-        
           <div>
             <img src={props.books[0].imgSrc} alt="" className="card-img" />
             <CardDetails>
               <p>{props.books[0].title}</p>
-              <p style={{fontWeight:"bold"}}>{props.books[0].space}</p>
-              </CardDetails>
-              <CardDetails>
+              <p style={{ fontWeight: "bold" }}>{props.books[0].space}</p>
+            </CardDetails>
+            <CardDetails>
               <p>{props.books[0].description}</p>
-              <p>{props.books[0].buttonText}</p>
-              </CardDetails>
+              <p>
+              <a
+                href={props.books[0].link}
+                className="card-btn"
+                target={props.books[0].target}
+              >
+                {props.books[0].buttonText}
+              </a>
+              </p>
+            </CardDetails>
           </div>
         </Card>
       </div>
